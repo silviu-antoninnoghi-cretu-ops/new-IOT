b04f5791e51de4546f462db67092f7b3e7a4dcf0
silviu-antoninnoghi-cretu-ops <silviu-antonin.noghi-cretu@student.tuiasi.ro>
2026-02-01 19:53:21 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -64,5 +64,6 @@ def get_sensor_data():
         return jsonify([])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
